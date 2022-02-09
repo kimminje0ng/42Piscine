1735b5b90aec6876d1f91a62f39cc418ea76a0c8
Minjeong Kim <kimmin9642@dgu.ac.kr>
2022-02-09 20:45:45 +0900

    ADD c02/ex11, 12

--- c02/ex11/ft_putstr_non_printable.c
+++ c02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: minjeki2 <minjeki2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 16:41:26 by minjeki2          #+#    #+#             */
-/*   Updated: 2022/02/08 21:17:37 by minjeki2         ###   ########.fr       */
+/*   Updated: 2022/02/09 20:40:30 by minjeki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,13 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+int	ft_str_is_printable(char c)
+{
+	if (c <= 31 || c >= 127)
+		return (0);
+	return (1);
+}
+
 void	ft_putstr_non_printable(char *str)
 {
 	char	*hex;
@@ -25,7 +32,7 @@ void	ft_putstr_non_printable(char *str)
 	hex = "0123456789abcdef";
 	while (*str)
 	{
-		if (ft_str_is_printable(str) == 0)
+		if (ft_str_is_printable(*str) == 0)
 		{
 			ft_putchar('\\');
 			if (*str < 0)
--- c02/ex12/ft_print_memory.c
+++ c02/ex12/ft_print_memory.c
@@ -6,7 +6,91 @@
 /*   By: minjeki2 <minjeki2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/08 16:41:38 by minjeki2          #+#    #+#             */
-/*   Updated: 2022/02/08 16:41:38 by minjeki2         ###   ########.fr       */
+/*   Updated: 2022/02/09 20:41:18 by minjeki2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	p_hex(char *str, char *hex)
+{
+	int	i;
+
+	i = 0;
+	while (i < 16)
+	{
+		if (!*str)
+		{
+			write(1, " ", 1);
+			break ;
+		}
+		write(1, &hex[*str / 16], 1);
+		write(1, &hex[*str % 16], 1);
+		if (i % 2 == 1)
+			write(1, " ", 1);
+		str++;
+		i++;
+	}
+	while (i++ < 16)
+	{
+		if (i % 2 == 1)
+			write(1, " ", 1);
+		write(1, "  ", 2);
+	}
+}
+
+void	p_str(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (i < 16)
+	{
+		if (!*str)
+			break ;
+		if (*str <= 31 || *str >= 127)
+			write(1, ".", 1);
+		else
+			write(1, *str, 1);
+		str++;
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+void	addr2hex(unsigned long long addr, unsigned int i, char *hex)
+{
+	if (addr != 0)
+	{
+		addr2hex(addr / 16, i + 1, hex);
+		write(1, &hex[addr % 16], 1);
+	}
+	else
+	{
+		while (i < 16)
+		{
+			write(1, "0", 1);
+			i++;
+		}
+	}
+}
+
+void	ft_print_memory(void *addr, unsigned int size)
+{
+	unsigned int	i;
+	char			*tmp;
+	char			*hex;
+
+	tmp = (char *)addr;
+	hex = "0123456789abcdef";
+	i = 0;
+	while (*tmp != 0 && size > i)
+	{
+		addr2hex((unsigned long long)tmp, 0, hex);
+		write(1, ": ", 2);
+		p_hex(tmp, hex);
+		p_str(tmp);
+		tmp += 16;
+		i += 16;
+	}
+}
